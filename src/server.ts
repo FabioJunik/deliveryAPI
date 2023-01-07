9770a364e49cc8d80af1fdc75b6602d0c6bd29e4
fabiojunik <fabiojunik@gmail.com>
2023-01-07 16:10:08 +0100

    feat: create error handler

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import { routes } from "./routes";
 
 
@@ -6,5 +7,18 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            message: err.message
+        });
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server error"
+    })
+});
 
 app.listen(3000, () => console.log("Server is runnig in port 3000"))
